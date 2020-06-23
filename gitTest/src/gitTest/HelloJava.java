70522ea44306b95bbe0b920c0d1d813586c78bbe
SM050005 <SM050005@DESKTOP-GDD5R2S>
2020-06-23 17:45:57 +0900

    소스코드수정

--- gitTest/src/gitTest/HelloJava.java
+++ gitTest/src/gitTest/HelloJava.java
@@ -3,7 +3,8 @@ package gitTest;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("HelloJava");
+		System.out.println("Hello");
 
 	}
 
